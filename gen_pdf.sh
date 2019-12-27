992447b14b2d838a853032b53c466e4b6877a00e
charlie-lee <hwholic0525@gmail.com>
2019-12-27 09:47:34 +0800

    update shell script

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -1,15 +1,42 @@
 #!/bin/sh
 
+# basic parameters
 filename=orb_slam2_notes
 ext=pdf
-hl_style=tango # {pygments, tango, espresso, zenburn, kate, monochrome, breezedark, haddock}
+dst_dir=~/Workspace/cygwinWorkspace/markdown/$filename
+options=$@ # input arguments
+str_echo="input options: gen, preview, clone"
 
-pandoc -s -f markdown+tex_math_single_backslash -t latex main.md chapters/*.md --pdf-engine=xelatex --highlight-style=$hl_style --filter pandoc-citeproc -o $filename.$ext
+# pandoc parameters
+template=/home/charlieli/.pandoc/default.latex
+# {pygments, tango, espresso, zenburn, kate, monochrome, breezedark, haddock}
+hl_style=tango
 
-#cp $filename.$ext ~/sf_D_DRIVE
+# script
+if [ -z "$options" ]; then # if options are empty
+    echo $str_echo
+else
+    for option in $options; do
+        if [ "$option" = "gen" ]; then
+            pandoc -s -f markdown+tex_math_single_backslash -t latex \
+                   main.md chapters/*.md \
+                   --pdf-engine=xelatex \
+                   --template=$template \
+                   --highlight-style=$hl_style \
+                   --filter pandoc-citeproc \
+                   -o $filename.$ext
+        elif [ "$option" = "preview" ]; then
+            cp $filename.$ext ~/sf_D_DRIVE
+        elif [ "$option" = "clone" ]; then
+            # remove dst dir and clone the current one
+            rm -rf $dst_dir
+            git clone . $dst_dir
+        else
+            echo $str_echo
+            break
+        fi
+    done
+fi
 
-## sync to windows (using git instead)
-#rsync -avh ../orb_slam2_notes ~/Workspace/cygwinWorkspace/markdown
-# remove dst dir and clone the current one
-rm -rf ~/Workspace/cygwinWorkspace/markdown/$filename
-git clone . ~/Workspace/cygwinWorkspace/markdown/$filename
+## sync to windows (use git instead)
+#rsync -avh ../SLAM_related_notes ~/Workspace/cygwinWorkspace/markdown
